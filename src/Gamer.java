b9735b7022985aec2f53c1240009403890ecca4c
JavaIn24 <yaracrut245@gmail.com>
2024-07-06 14:21:16 +0500

    Update Gamer.java
    q

--- src/Gamer.java
+++ src/Gamer.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Gamer {
+public class Gamer {
+     private String nickname;
+    private  boolean isActive;
+
+    public Gamer(String nickname, boolean isActive) {
+        this.nickname = nickname;
+        this.isActive = isActive;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public boolean isActive() {
+        return isActive;
+    }
 }
